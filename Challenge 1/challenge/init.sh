bc00b87e0a4f0097d484fd9718b0befdc882a6ec
abdoghazy2015 <64314534+abdoghazy2015@users.noreply.github.com>
2025-02-13 20:16:59 +0200

    Fix : D

--- Challenge 1/challenge/init.sh
+++ Challenge 1/challenge/init.sh
@@ -1,2 +1,2 @@
-echo "$FLAG" > /var/www/html/protected/flag.txt && unset FLAG;
+echo "<?php \$flag='$FLAG'; ?>" > /var/www/html/protected/flag.php && unset FLAG;
 service apache2 start && tail -f /dev/null
